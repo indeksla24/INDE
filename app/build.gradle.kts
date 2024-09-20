2ed995e5f903c14066bdca6883319ac0dbe81f07
krishana <indeksla24@gmail.com>
2024-09-20 17:44:13 +0530

    HALF PROJECT room design selection.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,17 +79,17 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-//  FILAMENT
-    implementation("com.google.android.filament:filament-android:1.9.9")
-    implementation("com.google.android.filament:gltfio-android:1.9.9")
-    implementation("com.google.android.filament:filament-utils-android:1.9.9")
-
-//  FIREBASE
-    implementation("com.google.firebase:firebase-database:21.0.0")
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
-    // Add the dependency for the Realtime Database library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-database")
+////  FILAMENT
+//    implementation("com.google.android.filament:filament-android:1.9.9")
+//    implementation("com.google.android.filament:gltfio-android:1.9.9")
+//    implementation("com.google.android.filament:filament-utils-android:1.9.9")
+//
+////  FIREBASE
+//    implementation("com.google.firebase:firebase-database:21.0.0")
+//    // Import the BoM for the Firebase platform
+//    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+//    // Add the dependency for the Realtime Database library
+//    // When using the BoM, you don't specify versions in Firebase library dependencies
+//    implementation("com.google.firebase:firebase-database")
 }
 
